d60047b73a94865b90678d44c3007ea441ff117d
Madeline Lei <madeline.lei@gmail.com>
2025-10-05 20:55:54 -0400

    add onboarding button

---
+++ src/components/Button.tsx
@@ -0,0 +1,9 @@
+import { Button } from '@mantine/core';
+
+export default function myButton(props: { label?: string, onClick: React.MouseEventHandler<HTMLButtonElement> }) {
+
+    return (
+        <Button variant="filled" color="green" size="sm" radius="lg" onClick={props.onClick}>{props.label}</Button>
+    )
+
+}
