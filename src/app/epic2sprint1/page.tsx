b9555669594b7de1f6430e7a9c2d43fa40d4251b
Cooper Golemme <116388624+coopergolemme@users.noreply.github.com>
2025-10-26 18:59:16 -0400

    feat: add layout to page

--- src/app/epic2sprint1/page.tsx
+++ src/app/epic2sprint1/page.tsx
@@ -1,5 +1,35 @@
-import { Title } from "@mantine/core";
+"use client";
+import TimelineSliderControls from "@/components/epic2sprint1/TimelineSliderControls";
+import { Box, Center, Paper, Stack, Title } from "@mantine/core";
 
 export default function Page() {
-  return <Title>Epic 2 Sprint 1 Page</Title>;
+  return (
+    <Stack align="space-between">
+      <Paper
+        p="xl"
+        radius="md"
+        withBorder>
+        <Title>Epic 2 Sprint 1 Page</Title>
+      </Paper>
+      <Paper
+        mt="md"
+        p="xl"
+        radius="md"
+        withBorder>
+        <Center>
+          <Title order={2}>Map will go here</Title>
+        </Center>
+        <Box h={500}></Box>
+      </Paper>
+      <Paper
+        p="xl"
+        radius="md"
+        withBorder>
+        <Stack>
+          <Title order={2}>Timeline Slider</Title>
+          <TimelineSliderControls />
+        </Stack>
+      </Paper>
+    </Stack>
+  );
 }
