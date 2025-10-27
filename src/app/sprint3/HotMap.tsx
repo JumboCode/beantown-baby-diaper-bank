3e1c193f60abf854560ea7965115606cd73894d9
Cooper Golemme <116388624+coopergolemme@users.noreply.github.com>
2025-10-26 20:39:50 -0400

    fix: linting error preventing deployment

--- src/app/sprint3/HotMap.tsx
+++ src/app/sprint3/HotMap.tsx
@@ -1,29 +1,32 @@
-"use client"
-import { useState, useEffect } from 'react';
-import { mapData } from './mockData';
-import ChoroplethMap from './ChoroplethMap';
-import { Select } from '@mantine/core';
+"use client";
+import { useState, useEffect } from "react";
+import { mapData } from "./mockData";
+import ChoroplethMap from "./ChoroplethMap";
+import { Select } from "@mantine/core";
 
 type MapDataKey = keyof typeof mapData;
 
 export default function HotMap() {
   const [geoData, setGeoData] = useState<GeoJSON.FeatureCollection>();
-  const [datasetKey, setDatasetKey] = useState<MapDataKey>('May 2025');
+  const [datasetKey, setDatasetKey] = useState<MapDataKey>("May 2025");
 
   const dataForSelectedPeriod = mapData[datasetKey];
 
   useEffect(() => {
-    fetch('/my-5-towns-wgs84-simplified.json')
-      .then(response => response.json())
-      .then(data => setGeoData(data))
-      .catch(error => console.error('Error loading JSON:', error));
+    fetch("/my-5-towns-wgs84-simplified.json")
+      .then((response) => response.json())
+      .then((data) => setGeoData(data))
+      .catch((error) => console.error("Error loading JSON:", error));
   }, []);
 
   return (
     <div>
-      <h2 style={{ fontWeight: 'bold', fontSize: '1.25rem' }}> Greater Boston Diapers Map </h2>
+      <h2 style={{ fontWeight: "bold", fontSize: "1.25rem" }}>
+        {" "}
+        Greater Boston Diapers Map{" "}
+      </h2>
       <Select
-        data={['May 2025', 'June 2025']}
+        data={["May 2025", "June 2025"]}
         value={datasetKey}
         onChange={(value) => {
           /* 'value' here is of type string | null */
@@ -32,21 +35,20 @@ export default function HotMap() {
             setDatasetKey(value as MapDataKey);
           }
         }}
-        style={{ width: '160px' }}
+        style={{ width: "160px" }}
       />
-      {
-        geoData ?
-          <ChoroplethMap 
-            width={600}
-            height={400}
-            geojson={geoData}
-            data={dataForSelectedPeriod}
-          />
-        :
-          <>
-            <p>Oops! The choropleth map can't be loaded at the moment</p>
-          </>
-      }
+      {geoData ? (
+        <ChoroplethMap
+          width={600}
+          height={400}
+          geojson={geoData}
+          data={dataForSelectedPeriod}
+        />
+      ) : (
+        <>
+          <p>Oops! The choropleth map can&apos;t be loaded at the moment</p>
+        </>
+      )}
     </div>
-  )
+  );
 }
--- src/app/sprint3/mockData.js
+++ src/app/sprint3/mockData.js
@@ -2,26 +2,26 @@
 
 export const mapData = {
   "June 2025": {
-    "BOSTON": 201,
-    "CAMBRIDGE": 83,
-    "SOMERVILLE": 77,
-    "MEDFORD": 53,
-    "ARLINGTON": 25,
+    BOSTON: 201,
+    CAMBRIDGE: 83,
+    SOMERVILLE: 77,
+    MEDFORD: 53,
+    ARLINGTON: 25,
   },
   "May 2025": {
-    "BOSTON": 150,
-    "CAMBRIDGE": 95,
-    "SOMERVILLE": 70,
-    "MEDFORD": 50,
-    "ARLINGTON": 30
+    BOSTON: 150,
+    CAMBRIDGE: 95,
+    SOMERVILLE: 70,
+    MEDFORD: 50,
+    ARLINGTON: 30,
   },
 };
 
 export const colors = [
-  "#feedde", 
-  "#fdd0a2", 
-  "#fdae6b", 
-  "#fd8d3c", 
-  "#e6550d", 
+  "#feedde",
+  "#fdd0a2",
+  "#fdae6b",
+  "#fd8d3c",
+  "#e6550d",
   "#a63603",
 ];
