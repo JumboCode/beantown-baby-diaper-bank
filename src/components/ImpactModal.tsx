4ef945b270ae34bfb1974eef1990c918fd4f899a
aryaam72 <147558291+aryaam72@users.noreply.github.com>
2025-10-29 00:27:41 -0400

    implemented changes

--- src/components/ImpactModal.tsx
+++ src/components/ImpactModal.tsx
@@ -1,5 +1,5 @@
 import { useDisclosure } from '@mantine/hooks';
-import { Modal, Button, NumberInput, Text, NavLink } from '@mantine/core';
+import { Modal, Button, NumberInput, Text, Mark } from '@mantine/core';
 import { useState } from 'react';
 
 export default function ImpactModal() {
@@ -9,23 +9,31 @@ export default function ImpactModal() {
   return (
     <>
       <Modal c="#138D8A" title="Make An Impact!" opened={opened} onClose={close} centered>
-        <NumberInput size="lg" label="Donation Amount" placeholder="$ USD" value={value} onChange={setValue} allowNegative={false}/>        
-        <Text size="md" style={{
-          padding: "3px",
-        }}>You can help 
-          <div className="p-2 mx-2 border-1 border-[##D0D5DD] inline-block rounded-md">{value && Number(value) * 4}</div>
-        families!</Text>
-        <NavLink style={{
-            backgroundColor: "#138D8A",
-            color: "white",
-            fontSize: "12px",
-            fontWeight: "bold",
-            borderRadius: "10px",
-            width: "auto",
-            display: "inline-flex",
-          }} href="https://beantownbabydiaperbank.org/donate/" label="Donate"/>
+        <NumberInput size="lg" label="Donation Amount" placeholder="$ USD" value={value} onChange={setValue} allowNegative={false}/>
+        {value !== undefined && value !== null && value !== '' && (
+          <Text size="md" style={{
+            padding: "3px",
+          }}>You can help 
+            <Mark p={1} m={4} bd="2px solid #138D8A" c="#138D8A" fw={600} bg="none"> {value && Number(value) * 4} </Mark>
+          families!
+          </Text>
+        )}    
+        <Button style={{
+          backgroundColor: "#138D8A",
+          color: "white",
+          fontSize: "12px",
+          fontWeight: "bold",
+          borderRadius: "10px",
+          width: "auto",
+          display: "inline-flex",
+        }}
+        component="a"
+        my="md" 
+        href="https://beantownbabydiaperbank.org/donate" 
+        target="_blank">
+          Donate
+        </Button>
       </Modal>
-
       <Button size="auto" variant="default" onClick={open}>
         Open Modal
       </Button>
