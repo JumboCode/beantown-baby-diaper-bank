b15f2fde31018d9b30ee2a290e477f12ec6f3059
Cooper Golemme <116388624+coopergolemme@users.noreply.github.com>
2025-10-30 15:19:39 -0400

    fix: convert prisma config to mjs to prevent ts errors

--- prisma.config.mjs
+++ prisma.config.mjs
@@ -1,5 +1,4 @@
 import "dotenv/config";
-import type { PrismaConfig } from "prisma";
 
 export default {
   engine: "classic",
@@ -8,9 +7,8 @@ export default {
     path: "prisma/migrations",
   },
   schema: "prisma/schema.prisma",
-  // now you can use process.env variables
   datasource: {
-    url: process.env.DATABASE_URL!,
-    directUrl: process.env.DIRECT_URL!,
+    url: process.env.DATABASE_URL,
+    directUrl: process.env.DIRECT_URL,
   },
-} satisfies PrismaConfig;
+};
