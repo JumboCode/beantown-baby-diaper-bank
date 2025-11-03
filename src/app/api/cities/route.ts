a2b1e3c1fd99757bcc2b98360de370f7c23b9e28
valenhenaog <henaog.valentina@gmail.com>
2025-11-03 12:42:04 -0500

    amazing database querying

--- src/app/api/cities/route.ts
+++ src/app/api/cities/route.ts
@@ -1,10 +1,74 @@
 // TODO: Implement city API endpoint
 
 import { NextResponse } from "next/server";
+import { prisma } from "@/lib/prisma";
+import { Prisma as PrismaTypes } from "@/generated/prisma/client";
+import type { City, Partner } from "@/generated/prisma/client";
 
-export async function GET() {
-  return NextResponse.json(
-    { message: "City API endpoint not implemented yet." },
-    { status: 501 }
-  );
+export async function GET(request: Request) {
+  const { searchParams } = new URL(request.url);
+
+  const cityName = searchParams.get("name");
+
+  const where: PrismaTypes.CityWhereInput = {};
+
+  if (cityName) {
+    where.name = {
+      contains: cityName,
+      mode: "insensitive",
+    };
+  }
+
+  try {
+    const cities: City[] = await prisma.city.findMany({
+      where,
+      orderBy: { name: "asc" },
+      include: {
+        distributions: {
+          include: {
+            partner: true,
+          }
+        },
+        partnerRegions: {
+          include: {
+            partner: true,
+          },
+        }
+      },
+    });
+
+    const dataToReturn = cities.map((city) => ({
+      id: Number(city.id),
+      name: city.name,
+      distributions: city.distributions.map((distribution) => ({
+        id: Number(distribution.id),
+        year: distribution.year,
+        month: distribution.month,
+        numberDiapers: Number(distribution.numberDiapers),
+        numberChildren: Number(distribution.numberChildren),
+        percentage: distribution.percentage,
+        partner: {
+          id: Number(distribution.partnerId),
+          name: distribution.partner.name
+        }
+      })),
+      partners: city.partnerRegions.map((partnerRegion) => ({
+        id: Number(partnerRegion.partnerId),
+        name: partnerRegion.partner.name,
+      })),
+    }));
+
+    return NextResponse.json(
+      { data: dataToReturn, }
+    );
+  } catch (error) {
+    const message = error instanceof Error
+      ? error.message
+      : `Unable to retrieve from the database.`;
+
+    return NextResponse.json(
+      { error: message },
+      { status: 500 },
+    );
+  }
 }
