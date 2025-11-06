d17996220fb21245d61f8ca615c2075bf52cdc1d
aryaam72 <147558291+aryaam72@users.noreply.github.com>
2025-11-06 18:31:43 -0500

    completed timeline slider w/ button

--- src/components/epic2sprint1/TimelineSliderControls.tsx
+++ src/components/epic2sprint1/TimelineSliderControls.tsx
@@ -1,22 +1,43 @@
 import { useState } from "react";
 import TimelineSlider from "./TimelineSlider";
+import {ActionIcon} from "@mantine/core";
 
 export default function TimelineSliderControls() {
   // Define state for the slider value and pass it to TimelineSlider
   // this pattern allows the controls you create here to modify the slider
   // state
   const [value, setValue] = useState(0);
+
+  // onClick function that adjusts the slider value and prevents overflow
+  // 1 for right click, -1 for left click
+  const moveSlider = (dir:number) => {
+    const max = 10;
+    const min = 0;
+
+    if (dir > 0) {
+      (value == max) ? setValue(value) : setValue(value + dir)
+    } else {
+      (value == min) ? setValue(value) : setValue(value + dir)
+    }
+  }
+
   return (
     <>
-      {/* Example of how you could implement the buttons to control the slider */}
-      {/* TODO: Implement the buttons to control the slider */}
-      {/* <LeftButton onClick={() => setValue(value - 1)} /> */}
-      <TimelineSlider
-        monthlyOrYearly="monthly" // Example value, adjust as needed
-        value={value}
-        setValue={setValue}
-      />
-      {/* <RightButton onClick={() => setValue(value + 1)} /> */}
+      <div className="flex flex-row items-center w-full justify-around">
+        <ActionIcon color="#053766" onClick={() => moveSlider(-1)}>
+          <img src="/timelineSlider/left.svg"></img>
+        </ActionIcon>
+        <div className="flex-1 px-5">
+          <TimelineSlider
+            monthlyOrYearly="monthly"
+            value={value}
+            setValue={setValue}
+          />
+        </div>
+        <ActionIcon color="#053766" onClick={() => moveSlider(1)}>
+          <img src="/timelineSlider/right.svg"></img>
+        </ActionIcon>
+      </div>
     </>
   );
 }
