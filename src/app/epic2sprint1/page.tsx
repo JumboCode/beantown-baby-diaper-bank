296df19740dedd0d25b27eb4e35a736a062390d9
Cooper Golemme <116388624+coopergolemme@users.noreply.github.com>
2025-11-09 18:31:36 -0500

    feat: add page for next sprint (#49)

--- src/app/epic2sprint1/page.tsx
+++ src/app/epic2sprint1/page.tsx
@@ -3,23 +3,33 @@ import TimelineSliderControls from "@/components/epic2sprint1/TimelineSliderCont
 import { Box, Center, Paper, Stack, Title } from "@mantine/core";
 
 import PartnerInfo from "./partnerInfo";
+import LeafletMap from "@/components/map/Map";
 
 export default function Page() {
   return (
     <Stack align="space-between">
-      <Paper p="xl" radius="md" withBorder>
+      <Paper
+        p="xl"
+        radius="md"
+        withBorder>
         <Title>Epic 2 Sprint 1 Page</Title>
       </Paper>
       <Paper>
         <PartnerInfo />
       </Paper>
-      <Paper mt="md" p="xl" radius="md" withBorder>
-        <Center>
-          <Title order={2}>Map will go here</Title>
-        </Center>
-        <Box h={500}></Box>
+      <Paper
+        mt="md"
+        p="xl"
+        radius="md"
+        withBorder>
+        <Box h={600}>
+          <LeafletMap />
+        </Box>
       </Paper>
-      <Paper p="xl" radius="md" withBorder>
+      <Paper
+        p="xl"
+        radius="md"
+        withBorder>
         <Stack>
           <Title order={2}>Timeline Slider</Title>
           <TimelineSliderControls />
