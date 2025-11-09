adb3d45e35b9564a1aca01ef7244a2809161fc2d
Caitlyn Wei <109092123+caitlynwei@users.noreply.github.com>
2025-11-09 13:03:12 -0500

    BBDB-25-DBtoPartnerInfo-CaitlynElchin (#46)
    * partner info buttons
    
    * styling partner info buttons
    
    * added fixes
    
    * fixed fr

--- src/app/epic2sprint1/page.tsx
+++ src/app/epic2sprint1/page.tsx
@@ -2,11 +2,16 @@
 import TimelineSliderControls from "@/components/epic2sprint1/TimelineSliderControls";
 import { Box, Center, Paper, Stack, Title } from "@mantine/core";
 
+import PartnerInfo from "./partnerInfo";
+
 export default function Page() {
   return (
     <Stack align="space-between">
       <Paper p="xl" radius="md" withBorder>
         <Title>Epic 2 Sprint 1 Page</Title>
+      </Paper>
+      <Paper>
+        <PartnerInfo />
       </Paper>
       <Paper mt="md" p="xl" radius="md" withBorder>
         <Center>
