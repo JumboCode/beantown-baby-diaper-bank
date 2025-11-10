12f55a305f883b51aa1bf5230181ef94f56a56cf
Cooper Golemme <116388624+coopergolemme@users.noreply.github.com>
2025-11-10 15:38:26 -0500

    fix: add adapter

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -8,13 +8,19 @@
  */
 import { PrismaClient } from "../generated/prisma/client";
 import { withAccelerate } from "@prisma/extension-accelerate";
+import { PrismaPg } from "@prisma/adapter-pg";
+
+const connectionString = `${process.env.DATABASE_URL}`;
+
+const adapter = new PrismaPg({ connectionString });
 
 const globalForPrisma = global as unknown as {
   prisma: PrismaClient;
 };
 
 const prisma =
-  globalForPrisma.prisma || new PrismaClient().$extends(withAccelerate());
+  globalForPrisma.prisma ||
+  new PrismaClient({ adapter }).$extends(withAccelerate());
 
 if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
 
