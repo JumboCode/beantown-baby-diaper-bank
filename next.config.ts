65a97ce4e7d723e38213a83199f242bfc47fdeb5
Cooper Golemme <116388624+coopergolemme@users.noreply.github.com>
2025-11-10 15:25:28 -0500

    fix: add experimental

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,8 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  outputFileTracingIncludes: {
-    "/": ["./src/generated/prisma/**"],
+const nextConfig = {
+  experimental: {
+    outputFileTracingIncludes: {
+      "/": ["./src/generated/prisma/**"],
+    },
   },
 };
 
