d7dc20804fc6afc0dd91c1703e921fb59f685c57
aryaam72 <147558291+aryaam72@users.noreply.github.com>
2025-12-04 19:47:03 -0500

    conditional rendering of month/range of months

--- src/components/admin/MonthSelectionModal.tsx
+++ src/components/admin/MonthSelectionModal.tsx
@@ -1,11 +1,16 @@
 import { useState } from 'react';
 import { MonthPickerInput } from '@mantine/dates';
 import { useDisclosure } from '@mantine/hooks';
-import { Modal, Button, Text } from '@mantine/core';
+import { Modal, Button, Text, Radio, RadioGroup, Group } from '@mantine/core';
 
 export default function MonthSelectionModal() {
   const [opened, { open, close }] = useDisclosure(false);
+  const [numMonths, setNumMonths] = useState('react');
   const [value, setValue] = useState<[string | null, string | null]>([null, null]);
+  
+  const handleMonthChange = () => {
+    setNumMonths();
+  };
 
   async function handleClick = () => {
     console.log(value)
@@ -14,7 +19,7 @@ export default function MonthSelectionModal() {
     console.log(start, end)
     try {
         // for now handles just the range, not the option of a particular month being selected
-        const response = await fetch("")
+        const response = await fetch("http://localhost:3000/api/distributions?month=May&year=2025")
     
     } catch {
 
@@ -29,13 +34,35 @@ export default function MonthSelectionModal() {
         withCloseButton={true} centered>
         {/* <Text size="xl">Delete Records</Text> */}
         <Text c="dimmed">Select a date range to preview and delete records.</Text>
-        <MonthPickerInput
+
+        <RadioGroup
+          label="Select your favorite framework/library"
+          description="This is anonymous"
+          value={monthsRange}
+          onChange={setMonthsRange}
+          required
+        >
+          <Radio value="one_month" label="One Month"/>
+          <Radio value="range_of_months" label="Range of Months"/>
+        </RadioGroup>
+        
+        
+        {/* /* /* <MonthPickerInput
             type="range"
             label="Pick dates range"
             placeholder="Pick dates range"
             value={value}
             onChange={setValue}
         />
+
+        <MonthPickerInput
+          dropdownType="modal"
+          label="Pick date"
+          placeholder="Pick date"
+          value={value}
+          onChange={setValue}
+        /> */ }
+
         <Button onClick={handleClick}>Apply Range</Button>
       </Modal>
 
