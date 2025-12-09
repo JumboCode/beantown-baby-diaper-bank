8ef21349437cafcf9ab295d84fc6f6a6fac7ac81
Cooper Golemme <116388624+coopergolemme@users.noreply.github.com>
2025-12-09 14:59:43 -0500

    change config

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,9 @@ const nextConfig: NextConfig = {
   outputFileTracingIncludes: {
     "/": ["./src/generated/prisma/**"],
   },
+  outputFileTracingExcludes: {
+    "/": ["./prisma/data/**"], // <- skip this directory
+  },
 };
 
 export default nextConfig;
