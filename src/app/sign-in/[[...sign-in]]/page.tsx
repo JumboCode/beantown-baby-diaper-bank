2548d8ba60cc41461400278f747e174e9129635d
Cooper Golemme <116388624+coopergolemme@users.noreply.github.com>
2026-02-01 19:31:27 -0500

    add clerk sign in component

--- src/app/sign-in/[[...sign-in]]/page.tsx
+++ src/app/sign-in/[[...sign-in]]/page.tsx
@@ -1,10 +1,12 @@
 import { SignIn } from "@clerk/nextjs";
-import { Center } from "@mantine/core";
+import { Center, Text } from "@mantine/core";
 
 export default function Page() {
   return <Center
   >
-
-    <SignIn />;
+    <Text
+    >
+      Your SignIn Component Here
+    </Text>
   </Center>
 }
