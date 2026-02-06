1f32ec45fc103427dcf57e5e360797cc6ab4eed7
Cooper Golemme <116388624+coopergolemme@users.noreply.github.com>
2026-02-06 14:52:50 -0500

    feat: add backend

---
+++ src/app/api/monthly-data/route.ts
@@ -0,0 +1,8 @@
+
+export async function GET() {
+    return new Response(JSON.stringify({ message: "Hello World" }));
+}
+
+export async function POST() {
+    return new Response(JSON.stringify({ message: "Hello World" }));
+}
