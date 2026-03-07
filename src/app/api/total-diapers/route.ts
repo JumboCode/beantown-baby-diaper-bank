e17d5484eefd0baf109c7f0dfdfb271bbaa8cb51
Cooper Golemme <116388624+coopergolemme@users.noreply.github.com>
2026-03-07 13:27:28 -0500

    fix: update total diapers route

--- src/app/api/total-diapers/route.ts
+++ src/app/api/total-diapers/route.ts
@@ -1,7 +1,7 @@
 import { NextResponse } from "next/server";
 import { prisma } from "@/lib/prisma";
 
-export const revalidate = 2592000;
+export const revalidate = 1200;
 
 export async function GET() {
   try {
