b251ada72665966f768adadd5037bd8958748089
Cooper Golemme <116388624+coopergolemme@users.noreply.github.com>
2026-03-09 18:09:29 -0400

    add back /api/partners to protected route

--- src/proxy.ts
+++ src/proxy.ts
@@ -3,7 +3,7 @@ import { NextResponse } from "next/server";
 
 const isAdminRoute = createRouteMatcher(["/admin(.*)"]);
 const isAdminApiRoute = createRouteMatcher([
-  // "/api/partners",
+  "/api/partners",
   "/api/partners/percentages(.*)",
   "/api/distributions(.*)",
 ]);
