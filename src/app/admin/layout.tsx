2b7a485dc3c44384cdcb21d9456cbcb1fde0cdc9
Cooper Golemme <116388624+coopergolemme@users.noreply.github.com>
2026-04-17 08:55:28 -0400

    improve header

--- src/app/admin/layout.tsx
+++ src/app/admin/layout.tsx
@@ -1,24 +1,29 @@
 import Profile from "@/components/admin/Profile";
 import { ClerkProvider } from "@clerk/nextjs";
-import { AppShell, AppShellHeader, AppShellMain, Group, Text } from "@mantine/core";
+import { AppShell, AppShellHeader, AppShellMain, Button, Group } from "@mantine/core";
+import { MapIcon } from "lucide-react";
+import Image from "next/image";
 import Link from "next/link";
 
 export default async function Layout({ children }: { children: React.ReactNode }) {
   return (
     <ClerkProvider>
-      <AppShell header={{ height: 116 }}>
-        <AppShellHeader px={64}>
-          <Group h="100%" justify="space-between">
+      <AppShell header={{ height: "80" }}>
+        <AppShellHeader px="sm">
+          <Group h="100%" justify="space-between" align="flex-start" p={"sm"}>
             <Link href="/admin" style={{ textDecoration: "none" }}>
-              <img
-              src="/beantown-logo.svg"
-              alt="Beantown Baby Diaper Bank"
-              style={{ height: 100, width: "auto" }}
-            />
+              <Image
+                src="/beantown-logo.svg"
+                alt="Beantown Baby Diaper Bank"
+                height={200}
+                width={200}
+              />
             </Link>
 
-            <Group gap="xs">
-              <Link href="/">View Map</Link>
+            <Group gap="xs" p="md">
+              <Button leftSection={<MapIcon />} size="sm">
+                <Link href="/">View Hot Map</Link>
+              </Button>
               <Profile />
             </Group>
           </Group>
